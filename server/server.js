606e1a049ff34fd841647884fee617a2b8ec8408
Esty Friedman <ehshpiner@gmail.com>
2023-09-07 11:24:20 +0300

    update server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ import userRouter from './Rourers/userRouter.js';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials:true}));  
+app.use(cors());  
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,16 +21,12 @@ app.use('/api/lists', listsRouter);
 app.use('/api/seed', seedRouter);
 app.use('/api/user', userRouter);
 
-const allowedOrigin = 'http://localhost:3000';
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', allowedOrigin);
-  next();
-});
-
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }).catch((err) => console.log(err));
+
+module.exports = app;
 
